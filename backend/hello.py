59db785552115e024de88161938d0b4b589fc46a
Sheeva <sheeva@pop-os.localdomain>
2023-02-25 18:59:52 +0600

    implemented chatbot

--- backend/hello.py
+++ backend/hello.py
@@ -13,7 +13,7 @@
 # if __name__ == '__main__':
 #     app.run()
 
-from flask import Flask
+from flask import Flask,request
 from flask_cors import CORS,cross_origin
 import numpy as np
 import tensorflow as tf
@@ -46,15 +46,91 @@ def my_profile():
     
 
 
+# data = pickle.load( open( "training_data", "rb" ) )
+# words = data['words']
+# classes = data['classes']
+# train_x = data['train_x']
+# train_y = data['train_y']
+# net = tflearn.input_data(shape=[None, len(train_x[0])])
+# net = tflearn.fully_connected(net, 8, restore=False)
+# net = tflearn.fully_connected(net, 8, restore=False)
+# net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax', restore=False)
+# net = tflearn.regression(net)
+# model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+# print("Loading the Model......")
+# # load our saved model
+# model.load('./model.tflearn')
+
+# with open('intents.json') as json_data:
+#     intents = json.load(json_data)
+    
+
+
+
+# def clean_up_sentence(sentence):
+#     # It Tokenize or Break it into the constituents parts of Sentense.
+#     sentence_words = nltk.word_tokenize(sentence)
+#     # Stemming means to find the root of the word.
+#     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+#     return sentence_words
+
+# # Return the Array of Bag of Words: True or False and 0 or 1 for each word of bag that exists in the Sentence
+# def bow(sentence, words, show_details=False):
+#     sentence_words = clean_up_sentence(sentence)
+#     bag = [0]*len(words)
+#     for s in sentence_words:
+#         for i,w in enumerate(words):
+#             if w == s:
+#                 bag[i] = 1
+#                 if show_details:
+#                     print ("found in bag: %s" % w)
+#     return(np.array(bag))
+
+# ERROR_THRESHOLD = 0.25
+# print("ERROR_THRESHOLD = 0.25")
+
+# def classify(sentence):
+#     # Prediction or To Get the Posibility or Probability from the Model
+#     results = model.predict([bow(sentence, words)])[0]
+#     # Exclude those results which are Below Threshold
+#     results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+#     # Sorting is Done because heigher Confidence Answer comes first.
+#     results.sort(key=lambda x: x[1], reverse=True)
+#     return_list = []
+#     for r in results:
+#         return_list.append((classes[r[0]], r[1])) #Tuppl -> Intent and Probability
+#     return return_list
+
+
+# def response(sentence, userID='123', show_details=False):
+#     results = classify(sentence)
+#     # That Means if Classification is Done then Find the Matching Tag.
+#     if results:
+#         # Long Loop to get the Result.
+#         while results:
+#             for i in intents['intents']:
+#                 # Tag Finding
+#                 if i['tag'] == results[0][0]:
+#                     # Random Response from High Order Probabilities
+#                     print("this is random"+random.choice(i['responses']))
+#                     return str(random.choice(i['responses']))
+
+#             results.pop(0)    
+
+# @api.route('/api/response')
+# @cross_origin(origin='*',headers=['Content-Type','Authorization'])
+# def my_response():
+#     return response("Thanks")
+
 data = pickle.load( open( "training_data", "rb" ) )
 words = data['words']
 classes = data['classes']
 train_x = data['train_x']
 train_y = data['train_y']
 net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 8, restore=False)
-net = tflearn.fully_connected(net, 8, restore=False)
-net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax', restore=False)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
 net = tflearn.regression(net)
 model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 print("Loading the Model......")
@@ -92,8 +168,12 @@ print("ERROR_THRESHOLD = 0.25")
 def classify(sentence):
     # Prediction or To Get the Posibility or Probability from the Model
     results = model.predict([bow(sentence, words)])[0]
+    print('Prediction or To Get the Posibility or Probability from the Model')
+    print(results)
     # Exclude those results which are Below Threshold
     results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    print('Results after exclusion')
+    print(results)
     # Sorting is Done because heigher Confidence Answer comes first.
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -104,6 +184,8 @@ def classify(sentence):
 
 def response(sentence, userID='123', show_details=False):
     results = classify(sentence)
+    print('Results after classification')
+    print(results)
     # That Means if Classification is Done then Find the Matching Tag.
     if results:
         # Long Loop to get the Result.
@@ -115,10 +197,18 @@ def response(sentence, userID='123', show_details=False):
                     print("this is random"+random.choice(i['responses']))
                     return str(random.choice(i['responses']))
 
-            results.pop(0)    
+            results.pop(0)
+    else:return "we have no answer"            
 
-@api.route('/api/response')
+@api.route('/api/chat/<text>',methods=['GET','POST','OPTIONS'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
-def my_response():
-    return response("Thanks")
+def my_response(text):
+    # text = request.args.get('message')
+    print(text)
+    resp = response(text)
+    print(resp)
+    return {"response": resp}
 
+# Run the app
+if __name__ == '__main__':
+    api.run()
--- components/ChatbotApi.jsx
+++ components/ChatbotApi.jsx
@@ -1,12 +1,34 @@
+import axios from 'axios';
+
 const API = {
     GetChatbotResponse: async message => {
       return new Promise(function(resolve, reject) {
         setTimeout(function() {
           if (message === "hi") resolve("Welcome to chatbot!");
-          else resolve("echo : " + message);
+          // else resolve("echo : " + message);
+          else{
+            axios({
+              method: "GET",
+              url:`http://localhost:5000/api/chat/${message}`,
+              headers: {'Access-Control-Allow-Origin': '*', 'X-Requested-With': 'XMLHttpRequest'},
+            })
+            .then((response) => {
+              const res =response.data
+              console.log(response.data);
+              // setReplyMessage(response.response);
+              resolve(res.response)
+            }).catch((error) => {
+              if (error.response) {
+                console.log(error.response)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+                }
+            })
+          }
         }, 2000);
       });
     }
+    
   };
   
   export default API;
